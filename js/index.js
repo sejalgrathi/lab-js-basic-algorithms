3697c762435368fc823483ef5b58f3e4ebc9e12b
Sejal Rathi <78018326+sejalgrathi@users.noreply.github.com>
2023-10-15 22:15:19 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 // Iteration 1: Names and Input
-//
+
+string hacker1,hacker2;
+console.log("The driver's name is XXXX");
+console.log("The navigator's name is YYYY");
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length) {
+  console.log("The driver has the longest name, it has ${hacker1.length} characters.");
+}
+else if(hacker1.length < hacker2.length)
+  console.log("It seems that the navigator has the longest name, it has ${hacker2.length} characters.");
+}
+else{
+  console.log("Wow, you both have equally long names, ${hacker1.length} characters!");
+}
 
 // Iteration 3: Loops
